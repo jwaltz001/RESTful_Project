edb1bbf9d5a264136d55e35ca706edc1b17fc5e8
Justin Waltz <justinwaltz@Justins-iMac.local>
2019-08-04 15:44:27 -0500

    got update and delete routes working and ejs edit page up

--- controllers/wineSessions.js
+++ controllers/wineSessions.js
@@ -27,6 +27,16 @@ router.get("/:id", (req,res) => {
         });
     });
 });
+    //edit
+router.get("/:id/edit", (req,res) => {
+    Wines.findById(req.params.id, (err,foundWine) => {
+        res.render("app/edit_wine.ejs",
+        {
+            wine: foundWine
+        });
+    });
+});
+
 //POST
     //create
 router.post("/", (req,res) => {
@@ -39,6 +49,22 @@ router.post("/", (req,res) => {
         res.redirect("/wines");
     });
 });
+//PUT
+router.put("/:id", (req,res) => {
+    if (req.body.buyAgain === "on") {
+        req.body.buyAgain = true;
+    }else{
+        req.body.buyAgain = false;
+    }
+    Wines.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedWine) => {
+        res.redirect("/wines");
+    });
+});
 
+router.delete("/:id", (req,res) => {
+    Wines.findByIdAndRemove(req.params.id, (err, deletedWine) => {
+        res.redirect("/wines");
+    });
+});
 
 module.exports = router;
